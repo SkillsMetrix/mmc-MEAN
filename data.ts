1f2e8d1e977d3cd14bfa8f003082c013d3d9e354
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-11 12:32:53 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -39,3 +39,49 @@ export class MovieEffects {
 
   constructor(private action$: Actions, private dataService: DataService) {}
 }
+
+
+----
+
+
+  import { createAction, props } from '@ngrx/store';
+import { Movie } from '../../Models/movie';
+
+export const getMovies = createAction('[Movie] Get movie');
+export const getMoviesSuccess = createAction(
+  '[Movie] Get movie success',
+  (movies: ReadonlyArray<Movie>) => ({ movies })
+  // props<{ movies: ReadonlyArray<Movie> }>()
+);
+export const addMovies = createAction(
+  '[Movie] Add movie',
+  (movie: Movie) => ({ movie })
+  // props<{ movie: Movie }>()
+);
+export const addMoviesSuccess = createAction(
+  '[Movie] Add movie success',
+  // props<{ movie: Movie }>(),
+  (movie: Movie) => ({ movie })
+);
+
+
+---
+  import { createReducer, on } from '@ngrx/store';
+import { Movie } from 'src/app/Models/movie';
+import {
+  addMovies,
+  addMoviesSuccess,
+  getMoviesSuccess,
+} from '../Actions/movie.action';
+
+export interface MovieState {
+  movies: ReadonlyArray<Movie>;
+}
+
+const initialState: ReadonlyArray<Movie> = [];
+
+export const movieReducer = createReducer(
+  initialState,
+  on(getMoviesSuccess, (state, { movies }) => [...movies]),
+  on(addMoviesSuccess, (state, { movie }) => [...state, movie])
+);
