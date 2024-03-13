adb2109370797e69f6d3d11e223db065aad416de
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-13 13:34:43 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,59 +1,33 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+const registry=require('./registry.json')
 
-
-const express= require('express')
-const amqp= require('amqplib/callback_api')
-const app= express();
-
-app.get('/customer',(req,res)=>{
-    let data={
-        id:101,
-        name:'User123',
-        email:'user@mail.com'
+router.all('/:apiName/:path', (req, res) => {
+    console.log(req.params.apiName);
+    if(registry.services[req.params.apiName]){
+    axios.get(registry.services[req.params.apiName].url + req.params.path).then(
+        (response) => {
+            console.log(registry.services[req.params.apiName].url + req.params.path);
+            
+        res.send(response.data)
+    })
+    }else{
+    res.send('No Api Found')
     }
-amqp.connect('amqp://localhost',function(err,conn){
-    conn.createChannel(function(err,ch){
-        const queue= 'message_queue_user'
-        const msg= JSON.stringify(data)
-        ch.assertQueue(queue,{durable:false})
-        ch.sendToQueue(queue,Buffer.from(msg))
-        console.log(`sent ${msg} to ${queue}`);
 
-    })
 })
-    res.send('customer services activated')
-})
-
-
-app.listen(4000,()=>{
-    console.log('server is ready');
-})
+module.exports = router
 
 
 
-
-
-
-
-const express= require('express')
-const amqp= require('amqplib/callback_api')
-const app= express();
-
-app.get('/product',(req,res)=>{
-
-    amqp.connect('amqp://localhost',function(err,conn){
-        const queue= 'message_queue_user'
-        conn.createChannel(function(err,ch){
-            ch.assertQueue(queue,{durable:false})
-            console.log('Waiting for the message from QUEUE');
-            ch.consume(queue,async function(msg){
-                console.log('Message from Queue  ',msg.content.toString());
-                await res.send(msg.content.toString())
-            },{noAck:true})
-        })
-    })
-    //res.send('product services activated')
-})
-
-app.listen(4001,()=>{
-    console.log('server is ready');
-})
+{
+    "services":{
+        "testapi": {
+            "apiName":"testapi",
+            "host": "http://localhost",
+            "port": 4000,
+            "url": "http://localhost:4000/"
+        }
+    }
+}
