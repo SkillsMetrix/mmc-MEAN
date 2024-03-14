94d883d7c0dd743212f1e605e5b83139337ae1ca
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-14 12:35:30 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -7,15 +7,7 @@
     "start": "node server.js",
     "orders": "node order-service/index.js"
   },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/ganny26/circuit-breaker-nodejs.git"
-  },
-  "author": "selvaganeshrajam@gmail.com",
-  "license": "ISC",
-  "bugs": {
-    "url": "https://github.com/ganny26/circuit-breaker-nodejs/issues"
-  },
+ 
   "dependencies": {
     "@mollitia/prometheus": "0.0.8",
     "express": "^4.18.1",
