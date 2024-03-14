8a6b1d2c3bd31ea73bd1ca952bb2e13755279e93
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-14 12:50:06 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,18 +1,32 @@
-{
-  "name": "circuit-breaker-nodejs",
-  "version": "1.0.0",
-  "description": "",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js",
-    "orders": "node order-service/index.js"
-  },
- 
-  "dependencies": {
-    "@mollitia/prometheus": "0.0.8",
-    "express": "^4.18.1",
-    "mollitia": "0.0.8",
-    "request": "^2.88.2"
-  },
 
+
+const express= require('express')
+const Order= require('./Order')
+
+const app=express()
+app.use(express.json())
+
+function loadData(){
+    var orderList= new Array(
+        new Order('101','laptop','electronics'),
+        new Order('102','mobile','electronics'), 
+        new Order('103','charger','electronics'),
+        new Order('104','soap','essentials')
+    )
+    return orderList
 }
+let orderList= loadData()
+
+app.get('/orders',(req,res) =>{
+    let orders=[]
+    let category=req.query.category
+    if(req.query.category){
+        orders=orderList.filter((item) => item.category===category)
+    }else {
+        orders=orderList
+    }
+    res.status(200).send(orders)
+})
+app.listen(9090,()=>{
+    console.log('server for order is ready');
+})
