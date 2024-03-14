2d535d12191bc15c5274c6b1f544287ca7dd344d
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-14 12:58:47 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,32 +1,11 @@
-
-
-const express= require('express')
-const Order= require('./Order')
-
-const app=express()
-app.use(express.json())
-
-function loadData(){
-    var orderList= new Array(
-        new Order('101','laptop','electronics'),
-        new Order('102','mobile','electronics'), 
-        new Order('103','charger','electronics'),
-        new Order('104','soap','essentials')
-    )
-    return orderList
+onst config = {
+  name: 'appCounter',
+  slidingWindowSize: 6, // Failure Rate Calculation is done on the last 6 iterations
+  minimumNumberOfCalls: 3, // 3 iterations are needed to start calculating the failure rate, and see if circuit should be opened or not
+  failureRateThreshold: 60, // If half of the iterations or more are failing, the circuit is switched to Opened state.
+  slowCallDurationThreshold: 500, // An iteration is considered as being slow if the iteration lasts more than 1s
+  slowCallRateThreshold: 50, // If at least 80% of the iterations are considered as being slow, the circuit is switched to Opened state.
+  permittedNumberOfCallsInHalfOpenState: 2, // When the circuit is in Half Opened state, the circuit accepts 2 iterations in this state.
+  openStateDelay: 10000, // The circuit stays in Opened state for 10s
+  halfOpenStateMaxDelay: 30000,
 }
-let orderList= loadData()
-
-app.get('/orders',(req,res) =>{
-    let orders=[]
-    let category=req.query.category
-    if(req.query.category){
-        orders=orderList.filter((item) => item.category===category)
-    }else {
-        orders=orderList
-    }
-    res.status(200).send(orders)
-})
-app.listen(9090,()=>{
-    console.log('server for order is ready');
-})
