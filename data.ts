6446508885448b30431e0c834ed7a0d38619291f
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-14 13:02:13 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,4 +1,16 @@
-onst config = {
+const express = require('express')
+const httpRequest = require('request')
+
+const MollitiaPrometheus = require('@mollitia/prometheus')
+const Mollitia = require('mollitia')
+
+const app = express()
+const { Circuit, Fallback, SlidingCountBreaker, BreakerState } = Mollitia
+
+app.use(express.json({ urlencoded: true }))
+Mollitia.use(new MollitiaPrometheus.PrometheusAddon())
+
+const config = {
   name: 'appCounter',
   slidingWindowSize: 6, // Failure Rate Calculation is done on the last 6 iterations
   minimumNumberOfCalls: 3, // 3 iterations are needed to start calculating the failure rate, and see if circuit should be opened or not
@@ -9,3 +21,80 @@ onst config = {
   openStateDelay: 10000, // The circuit stays in Opened state for 10s
   halfOpenStateMaxDelay: 30000,
 }
+
+
+const slidingCountBreaker = new SlidingCountBreaker(config)
+
+const fallback = new Fallback({
+  callback(err) {
+    // Every time the method rejects, You can filter here
+    if (err) {
+      return err.message
+    }
+  },
+})
+
+ const orderCircuit = new Circuit({
+  name: 'Order Operations',
+  options: {
+    prometheus: {
+      name: 'orderCircuit',
+    },
+    modules: [slidingCountBreaker, fallback],
+  },
+})
+
+const orderController = {
+  getOrders: (category) => {
+    return new Promise((resolve, reject) => {
+      httpRequest(
+        {
+          uri: `http://localhost:9090/orders?category=${category}`,
+          method: 'GET',
+        },
+        (error, response, body) => {
+          if (error) {
+            reject(error)
+          } else if (response) {
+            if (response.statusCode === 200) {
+              resolve(JSON.parse(body))
+            } else {
+              resolve(response.body)
+            }
+          }
+        }
+      )
+    })
+  },
+}
+
+app.get('/stats', (req, res) => res.send(MollitiaPrometheus.metrics()))
+
+app.get('/orders', (req, res) => {
+  orderCircuit
+    .fn(() => orderController.getOrders(req.query.category))
+    .execute()
+    .then((result) => {
+      console.log('Circuit State -->', slidingCountBreaker.state)
+      res.send(result)
+    })
+    .catch((error) => {
+      console.log('Circuit State -->', slidingCountBreaker.state)
+      if (slidingCountBreaker.state === BreakerState.CLOSED) {
+        res.send({
+          status: false,
+          message: "Order service is down",
+        })
+      } else {
+        // Fallback Order response
+        res.send([
+          { id: '1', name: 'mobile', category: 'electronics', color: 'white', price: 20000 },
+          { id: '4', name: 'Laptop', category: 'electronics', color: 'gray', price: 50000 },
+        ])
+      }
+    })
+})
+
+app.listen(3000, () => {
+  console.log('server is running on port 3000')
+})
