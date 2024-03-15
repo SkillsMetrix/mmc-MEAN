e9971bb37f4eac8356147bd56c4e25597242b471
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-15 09:58:23 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,33 +1,47 @@
-const options = {
-  definition: {
-    openapi: "3.1.0",
-    info: {
-      title: "Node Express API with Swagger",
-      version: "0.1.0",
-      description:
-        "This is a simple CRUD API application made with Express and documented with Swagger",
-      license: {
-        name: "MIT",
-        url: "https://spdx.org/licenses/MIT.html",
-      },
-      contact: {
-        name: "Node",
-        url: "https://Node.com",
-        email: "info@email.com",
-      },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
-  },
-  apis: ["./routes/*.js"],
-};
+var express= require('express')
+var bp= require('body-parser')
+const swaggerJsdoc= require('swagger-jsdoc')
+const swaggerUi= require('swagger-ui-express')
 
-const specs = swaggerJsdoc(options);
+const app= express()
+
 app.use(
-  "/api-docs",
-  swaggerUi.serve,
-  swaggerUi.setup(specs)
-);
+    bp.urlencoded({extended:true}))
+    app.use(bp.json());
+    const options = {
+        definition: {
+          openapi: "3.1.0",
+          info: {
+            title: "Node Express API with Swagger",
+            version: "0.1.0",
+            description:
+              "This is a simple CRUD API application made with Express and documented with Swagger",
+            license: {
+              name: "MIT",
+              url: "https://spdx.org/licenses/MIT.html",
+            },
+            contact: {
+              name: "Node",
+              url: "https://Node.com",
+              email: "info@email.com",
+            },
+          },
+          servers: [
+            {
+              url: "http://localhost:4000",
+            },
+          ],
+        },
+        apis: ["./routes/*.js"],
+      };
+      
+      const specs = swaggerJsdoc(options);
+      app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(specs)
+      );
+
+app.listen(4000,()=>{
+    console.log('server is ready...!');
+})
