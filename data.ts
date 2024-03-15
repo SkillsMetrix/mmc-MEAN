ab79a9a88bee7152def667b11fbf69b996964454
skillsmetrix Technologies <161203473+SkillsMetrix@users.noreply.github.com>
2024-03-15 10:54:25 +0530

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,180 +1,65 @@
+router.get("/", function (req, res) {
+  res.status(200).json(books);
+});
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Book:
- *       type: object
- *       required:
- *         - title
- *         - author
- *         - finished
- *       properties:
- *         id:
- *           type: string
- *           description: The auto-generated id of the book
- *         title:
- *           type: string
- *           description: The title of your book
- *         author:
- *           type: string
- *           description: The book author
- *         finished:
- *           type: boolean
- *           description: Whether you have finished reading the book
- *         createdAt:
- *           type: string
- *           format: date
- *           description: The date the book was added
- *       example:
- *         id: d5fE_asz
- *         title: The New Turing Omnibus
- *         author: Alexander K. Dewdney
- *         finished: false
- *         createdAt: 2020-03-10T04:05:06.157Z
- */
+router.get("/:id", function (req, res) {
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
 
-/**
- * @swagger
- * tags:
- *   name: Books
- *   description: The books managing API
- * /books:
- *   get:
- *     summary: Lists all the books
- *     tags: [Books]
- *     responses:
- *       200:
- *         description: The list of the books
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/Book'
- *   post:
- *     summary: Create a new book
- *     tags: [Books]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
- *     responses:
- *       200:
- *         description: The created book.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Book'
- *       500:
- *         description: Some server error
- * /books/{id}:
- *   get:
- *     summary: Get the book by id
- *     tags: [Books]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The book id
- *     responses:
- *       200:
- *         description: The book response by id
- *         contens:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Book'
- *       404:
- *         description: The book was not found
- *   put:
- *    summary: Update the book by the id
- *    tags: [Books]
- *    parameters:
- *      - in: path
- *        name: id
- *        schema:
- *          type: string
- *        required: true
- *        description: The book id
- *    requestBody:
- *      required: true
- *      content:
- *        application/json:
- *          schema:
- *            $ref: '#/components/schemas/Book'
- *    responses:
- *      200:
- *        description: The book was updated
- *        content:
- *          application/json:
- *            schema:
- *              $ref: '#/components/schemas/Book'
- *      404:
- *        description: The book was not found
- *      500:
- *        description: Some error happened
- *   delete:
- *     summary: Remove the book by id
- *     tags: [Books]
- *     parameters:
- *       - in: path
- *         name: id
- *         schema:
- *           type: string
- *         required: true
- *         description: The book id
- *
- *     responses:
- *       200:
- *         description: The book was deleted
- *       404:
- *         description: The book was not found
- */
+  book ? res.status(200).json(book) : res.sendStatus(404);
+});
 
+router.post("/", function (req, res) {
+  const { title, author, finished } = req.body;
 
------
-
-  const books = [
-  {
-    id: 1,
-    title: "The Pragmatic Programmer",
-    author: "Andy Hunt / Dave Thomas",
-    finished: true,
+  let book = {
+    id: books.length + 1,
+    title: title,
+    author: author,
+    finished: finished !== undefined ? finished : false,
     createdAt: new Date(),
-  },
-  {
-    id: 2,
-    title: "Clean Code",
-    author: "Robert Cecil Martin",
-    finished: true,
-    createdAt: new Date(),
-  },
-  {
-    id: 3,
-    title: "The Art of Computer Programming",
-    author: "Donald Knuth",
-    finished: false,
-    createdAt: new Date(),
-  },
-  {
-    id: 4,
-    title: "Head First Design Patterns",
-    author: "Elisabeth Freeman / Kathy Sierra",
-    finished: true,
-    createdAt: new Date(),
-  },
-  {
-    id: 5,
-    title: "JavaScript: The Good Parts",
-    author: "Douglas Crockford",
-    finished: false,
-    createdAt: new Date(),
-  },
-];
+  };
 
-module.exports = books;
+  books.push(book);
 
+  res.status(201).json(book);
+});
+
+router.put("/:id", function (req, res) {
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
+
+  if (book) {
+    const { title, author, finished } = req.body;
+
+    let updated = {
+      id: book.id,
+      title: title !== undefined ? title : book.title,
+      author: author !== undefined ? author : book.author,
+      finished: finished !== undefined ? finished : book.finished,
+      createdAt: book.createdAt,
+    };
+
+    books.splice(books.indexOf(book), 1, updated);
+
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+router.delete("/:id", function (req, res) {
+  let book = books.find(function (item) {
+    return item.id == req.params.id;
+  });
+
+  if (book) {
+    books.splice(books.indexOf(book), 1);
+  } else {
+    return res.sendStatus(404);
+  }
+
+  res.sendStatus(204);
+});
